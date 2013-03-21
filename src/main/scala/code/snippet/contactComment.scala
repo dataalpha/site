67d337c7a659e225b0305d109c408e06f5c90e48
Byron Gibson <fbg111@gmail.com>
2013-03-20 18:36:51 -0700

    added contactContact snippet to index.html.  made minor change to copy in security (Prelaunch)

---
+++ src/main/scala/code/snippet/contactComment.scala
@@ -0,0 +1,7 @@
+package code.snippet
+
+import scala.xml.Unparsed
+
+object contactComment {
+  def render = Unparsed("""<!-- Smart people always read the fine print, and they always check the source code.  Let's chat, email me at byron at the same domain. -->""")
+}
