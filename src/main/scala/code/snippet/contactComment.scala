09a8a75d2045b8f1ac331339c3b8729f3b3041bd
Byron Gibson <fbg111@gmail.com>
2013-03-21 07:29:16 -0700

    minor copy change

--- src/main/scala/code/snippet/contactComment.scala
+++ src/main/scala/code/snippet/contactComment.scala
@@ -3,5 +3,5 @@ package code.snippet
 import scala.xml.Unparsed
 
 object contactComment {
-  def render = Unparsed("""<!-- Smart people always read the fine print, and they always check the source code.  Let's chat, email me at byron at the same domain. -->""")
+  def render = Unparsed("""<!-- Smart people always read the fine print, and they always inspect the source code.  Let's chat, email me at byron at the same domain. -->""")
 }
